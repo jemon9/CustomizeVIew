5857754fc4d0fce44e9fae1a9bdde66cdf5e54b7
jemon9 <zhangzhifu926@163.com>
2017-02-24 18:02:42 +0800

    add appwidgetprovider

---
+++ app/src/main/java/com/example/heyha/customizeview/ui/MyAppWidgetProvider.java
@@ -0,0 +1,105 @@
+package com.example.heyha.customizeview.ui;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.util.Log;
+import android.widget.RemoteViews;
+import com.example.heyha.customizeview.R;
+
+/**
+ * Created by Heyha on 2017/2/23.
+ */
+public class MyAppWidgetProvider extends AppWidgetProvider {
+    private static final String TAG = "MyAppWidgetProvider";
+    private static final String CLICK_ACTION = "com.example.heyha.ipc.action_Click";
+
+    public MyAppWidgetProvider() {
+        super();
+    }
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        super.onReceive(context, intent);
+        Log.d(TAG,"onReceive()");
+        if (intent.getAction().equals(CLICK_ACTION)){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Bitmap srcBitmap = BitmapFactory.decodeResource(context.getResources(), R.mipmap.widget);
+                    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                    for (int i = 0; i < 37; i++){
+                        float degree = (i * 10) % 360;
+                        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.appwidget_provider_layout);
+                        remoteViews.setImageViewBitmap(R.id.image_view,rotateBitmap(context,srcBitmap,degree));
+                        Intent intent1 = new Intent();
+                        intent1.setAction(CLICK_ACTION);
+                        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,0,intent1,0);
+                        remoteViews.setOnClickPendingIntent(R.id.image_view,pendingIntent);
+                        appWidgetManager.updateAppWidget(new ComponentName(context,MyAppWidgetProvider.class),remoteViews);
+//                        SystemClock.sleep(10);
+                    }
+                }
+            }).start();
+        }
+    }
+
+    private Bitmap rotateBitmap(Context context, Bitmap srcBitmap, float degree) {
+        Bitmap destBitmap;
+        Matrix matrix = new Matrix();
+        matrix.reset();
+        matrix.setRotate(degree);
+        destBitmap = Bitmap.createBitmap(srcBitmap,0,0,srcBitmap.getWidth(),srcBitmap.getHeight(),matrix,true);
+        return destBitmap;
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.d(TAG,"onUpdate()");
+        final int counter = appWidgetIds.length;
+        for (int i = 0; i < counter; i++){
+            onWidgetUpdate(context,appWidgetManager,appWidgetIds[i]);
+        }
+    }
+
+    private void onWidgetUpdate(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.appwidget_provider_layout);
+        //“桌面小部件”，单击事件发送的Intent广播
+        Intent intent = new Intent();
+        intent.setAction(CLICK_ACTION);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,0,intent,0);
+        remoteViews.setOnClickPendingIntent(R.id.image_view,pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        Log.d(TAG,"onDeleted()");
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d(TAG,"onEnabled()");
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        Log.d(TAG,"onDisabled()");
+    }
+
+    @Override
+    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+        super.onRestored(context, oldWidgetIds, newWidgetIds);
+        Log.d(TAG,"onRestored()");
+    }
+}
